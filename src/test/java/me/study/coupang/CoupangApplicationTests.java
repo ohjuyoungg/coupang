a63bc6cf690884da91f099defde156e8b885a81a
ohjuyoungg <ejrm12@naver.com>
2025-01-01 19:00:42 +0900

    Initial commit

---
+++ src/test/java/me/study/coupang/CoupangApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.coupang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
